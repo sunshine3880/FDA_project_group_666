fadcbf45e6c8a17059b7f249f7d11c05d14bebc1
hqq1995 <1034487856@qq.com>
2017-05-24 03:39:15 -1000

    Update FDA_code.R

--- FDA_code.R
+++ FDA_code.R
@@ -60,3 +60,17 @@ acf(mort)
 plot(mort,type='l') ## Indicates strong serial correlations (unit root)  
 dhpi=diff(hpi)  ## Working on differenced data 
 acf(dhpi)
+> m4
+
+Call:
+arima(x = dhpi, order = c(12, 0, 0), include.mean = F)
+
+Coefficients:
+         ar1      ar2      ar3     ar4      ar5      ar6     ar7     ar8      ar9
+      1.2343  -0.2827  -0.3300  0.3316  -0.0762  -0.1727  0.1563  0.0020  -0.0972
+s.e.  0.0434   0.0687   0.0696  0.0710   0.0725   0.0725  0.0724  0.0729   0.0713
+        ar10    ar11     ar12
+      0.1159  0.3202  -0.2325
+s.e.  0.0698  0.0689   0.0436
+
+sigma^2 estimated as 0.04963:  log likelihood = 38.27,  aic = -50.54
